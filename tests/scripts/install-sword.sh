3355fa60e152d0e283594bcd7bd01f9a506f54f5
Gary Holmlund <gary.holmlund@gmail.com>
2018-11-05 14:27:53 -0800

    Delete BibleTime desktop specific files.

--- tests/scripts/install-sword.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ve
-svn checkout --non-interactive http://crosswire.org/svn/sword/trunk sword-trunk
-cd sword-trunk
-mkdir b
-cd b
-cmake .. -DCMAKE_INSTALL_PREFIX="${HOME}/local"
-make "$@"
-make install
